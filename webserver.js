b53252bd86a12dc32107f1d8119ac448d946b991
Robin Muff <Robin_Muff1@sluz.ch>
2020-11-25 19:55:35 +0100

    API for Tasks

--- webserver.js
+++ webserver.js
@@ -3,7 +3,7 @@ import { Application, Router, send } from "https://deno.land/x/oak@v6.3.1/mod.ts
 const app = new Application();
 const router = new Router();
 
-let tasks = [];
+let tasks = [{Id: 0, Title: "LOL", Column: 0}];
 
 router 
     .get("/", (context) => {
@@ -12,6 +12,42 @@ router
     .get("/files/:fileName", (context) => {
         const fileName = context.params.fileName;
         return send(context, "Frontend/" + fileName)
+    })
+    
+    .get("/Tasks", (context) => {
+        context.response.body = tasks;
+    })
+    .post("/Tasks", async (context) => {
+        const task = await context.request.body({ type: "json" }).value;
+        tasks = [
+            ...tasks,
+            task
+        ];
+
+        context.response.status = 200
+        context.response.body = { message: 'OK' }
+    })
+    .delete("/Tasks:id", async context => {
+        const id = await context.params.id.substring(1);
+        let newtasks = []
+        tasks.forEach(element => {
+            if (element.Id != id) {
+                newtasks.push(element);
+            }
+        });
+        tasks = newtasks;
+
+        context.response.status = 200
+        context.response.body = { message: 'OK' }
+    })
+    .put("/Tasks:id", async context => {
+        const id = context.params.id.substring(1);
+        const task = await context.request.body({ type: "json" }).value;
+        const index = tasks.findIndex(p => p.Id == id);
+        tasks[index] = task;
+
+        context.response.status = 200
+        context.response.body = { message: 'OK' }
     })
 app.use(router.routes());
 app.listen({ port: 8000 });
